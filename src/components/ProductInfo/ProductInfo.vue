85845e9909eaddc6f735454ddcf61ebe2c3abcca
flyinglove <446419676@qq.com>
2018-08-21 00:24:13 +0800

    init commit

---
+++ src/components/ProductInfo/ProductInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="product-info">商品详情页</div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import {PRODUCT_INFO} from '../../conf/api'
+
+  export default {
+    name: 'ProductInfo',
+    created () {
+      axios.get(PRODUCT_INFO).then((res) => {
+        console.log(res)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/conf/api.js
@@ -0,0 +1,7 @@
+let env = process.env.NODE_ENV
+let API_DOMAIN
+if (env === 'development') {
+  API_DOMAIN = 'http://yapi.demo.qunar.com/mock/15250/'
+}
+
+export const PRODUCT_INFO = `${API_DOMAIN}group/product`
